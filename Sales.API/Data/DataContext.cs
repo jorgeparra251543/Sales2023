b1de87e516b4524bb4fd355a82c1e4e7f518305f
Jorge Luis Parra <jorge.parra@emast.co>
2023-02-26 15:30:21 -0500

    Proceso doker

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    //DataContext hereda de la clase DbContext que importamos(using Microsoft.EntityFrameworkCore;)
+    public class DataContext : DbContext
+    {
+        //conectarme a la bd, con el costructor, option se lo paso a la clase que hereda base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        
+        }
+
+        //Propiedades de la entidad
+        public DbSet<Country> Countries { get; set; }
+
+        //para que cada pais sea unico y no haya repetidos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();//creamos definicion para la bd
+
+        }
+
+
+    }
+}
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    //Entidad country, tabla en BD
+    public class Country
+    {
+        public int Id { get; set; } //identificador unico del Pais/Country
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //campo name obligatorio en {0} se coloca name, pero como definique es pais, sale pais
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")] //cantidad de caracteres, en caso de error {1} se reemplaza por 100
+        [Display(Name = "País")] // nombre que se vializa en la intefaz de usuario
+        public string Name { get; set; } = null!; //nombre del pais(debe ser diferente de null)
+    }
+}

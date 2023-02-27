07068cfdbb71e8c24860e2c0162ac697fa2b77d5
Jorge Luis Parra <jorge.parra@emast.co>
2023-02-26 20:14:40 -0500

    Segundo video

---
+++ Sales.WEB/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+
+namespace Sales.WEB.Repositories
+{
+    //esta T es reemplazable por objeto country,city
+    public class HttpResponseWrapper<T>
+    {
+        //contrcutor
+        public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            Error = error;
+            Response = response;
+            HttpResponseMessage = httpResponseMessage;
+        }
+
+        //propiedades publicos
+        public bool Error { get; set; }
+
+        public T? Response { get; set; }
+
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+
+        //metodos privados
+        public async Task<string?> GetErrorMessage()
+        {
+            if (!Error)
+            {
+                return null;
+            }
+
+            var statusCode = HttpResponseMessage.StatusCode;
+            if (statusCode == HttpStatusCode.NotFound)
+            {
+                return "Recurso no encontrado";
+            }
+            else if (statusCode == HttpStatusCode.BadRequest)
+            {
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+            }
+            else if (statusCode == HttpStatusCode.Unauthorized)
+            {
+                return "Tienes que logearte para hacer esta operación";
+            }
+            else if (statusCode == HttpStatusCode.Forbidden)
+            {
+                return "No tienes permisos para hacer esta operación";
+            }
+
+            return "Ha ocurrido un error inesperado";
+        }
+    }
+}
+
+
